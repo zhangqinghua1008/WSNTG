bacde92620feb30ded407a834e98aaaa3a544351
zqh <630762329@qq.com>
2021-11-05 16:17:44 +0800

    默认值

--- scripts/count_points.py
+++ scripts/count_points.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.add_argument('points_path', help='Path to point annotations')
+parser.add_argument('--points_path', default=r'G:\py_code\pycharm_Code\WESUP-TGCN\data_glas\train\points',
+                    help='Path to point annotations')
 args = parser.parse_args()
 
 print(np.mean([len(pd.read_csv(csv_file)) for csv_file in glob.glob(os.path.join(args.points_path, '*.csv'))]))
