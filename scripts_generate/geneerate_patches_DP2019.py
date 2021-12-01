64ebab708ccb3b63ac4a7a13f6dda40764d00fad
zqh <630762329@qq.com>
2021-12-01 17:19:16 +0800

    生存patch

--- scripts_generate/geneerate_patches_DP2019.py
+++ scripts_generate/geneerate_patches_DP2019.py
@@ -4,9 +4,8 @@
 # @File   : geneerate_patches_DP2019.py
 # @Note   :
 
-
 """
-    把Digestpath数据集切分成pacth
+    把Digestpath数据集中的WSI 图像 切分成pacth。
 """
 
 import sys
@@ -27,9 +26,39 @@ def resize_img(img, target_size):
     img = resize(img, target_size, order=1, anti_aliasing=False)
     return (img * 255).astype('uint8')
 
+# 移动文件
+def move_fire(ori_path, save_path, suffix=""):
+    img_dir = Path(ori_path) / "img"
+    label_dir = Path(ori_path) / "labelcol"
+
+    save_img_dir = Path(save_path) / ("img" + suffix)
+    save_img_dir.mkdir(exist_ok=True)
+    save_label_dir = Path(save_path) / ("labelcol" + suffix)
+    save_label_dir.mkdir(exist_ok=True)
+    # 转移img
+    for fire in sorted(img_dir.iterdir()):
+        img_path = save_img_dir / fire.name
+        shutil.copyfile(fire, img_path)  # 拷贝文件
+
+    # 转移label
+    for fire in sorted(label_dir.iterdir()):
+        img_path = save_label_dir / fire.name
+        shutil.copyfile(fire, img_path)  # 拷贝文件
+
+# 获取保存地址
+def save_dir(output):
+    output = Path(output)
+    output.mkdir(exist_ok=True)
+    target_img_dir = output / 'img'
+    target_mask_dir = output / 'labelcol'
+    target_img_dir.mkdir(exist_ok=True)
+    target_mask_dir.mkdir(exist_ok=True)
+    return target_img_dir, target_mask_dir
+
+
 def process_img_and_mask(img_path, mask_path, target_img_dir, target_mask_dir, need_filter=False, n_patches=12):
     '''
-        随机挑选patch
+        在pos类中 随机挑选patch
         # need_filter : 需要筛选（也就是选取有意义patch）
     '''
     Image.MAX_IMAGE_PIXELS = None
@@ -66,6 +95,10 @@ def process_img_and_mask(img_path, mask_path, target_img_dir, target_mask_dir, n
 
 
 def process_img_and_mask_neg(img_path, target_img_dir, target_mask_dir, n_patches=6, need_filter=False):
+    '''
+         在neg类中 随机挑选patch
+         # need_filter : 需要筛选（也就是选取有意义patch）
+     '''
     Image.MAX_IMAGE_PIXELS = None
     img = io.imread(img_path)
     h, w = img.shape[:2]
@@ -96,18 +129,8 @@ def process_img_and_mask_neg(img_path, target_img_dir, target_mask_dir, n_patche
         io.imsave(str(target_mask_dir / patch_name), mask, check_contrast=False)
 
 
-def save_dir(output):
-    output = Path(output)
-    output.mkdir(exist_ok=True)
-    target_img_dir = output / 'img'
-    target_mask_dir = output / 'labelcol'
-    target_img_dir.mkdir(exist_ok=True)
-    target_mask_dir.mkdir(exist_ok=True)
-    return target_img_dir, target_mask_dir
-
-
 # 切分主要运行函数
-def fun(dataset_path, output, class_type="pos", need_filter=False):
+def run(dataset_path, output, class_type="pos", need_filter=False):
     dataset_path = Path(dataset_path)  # 样本地址
     img_dir = dataset_path / 'images'
     mask_dir = dataset_path / 'masks'
@@ -122,9 +145,7 @@ def fun(dataset_path, output, class_type="pos", need_filter=False):
     # 获得保存地址
     target_img_dir, target_mask_dir = save_dir(output)
 
-    print('\nSplitting into patches 分裂成补丁 ...')
-    # for img_path, mask_path in tqdm(zip(img_paths, mask_paths), total=len(img_paths)):
-    # process_img_and_mask(img_path, mask_path,target_img_dir,target_mask_dir,n_patches=10)
+    print('\nSplitting into patches 分裂成patch ...')
 
     # 并行计算
     executor = Parallel(n_jobs=12)
@@ -138,59 +159,30 @@ def fun(dataset_path, output, class_type="pos", need_filter=False):
                                                    need_filter=need_filter)
                  for img_path in tqdm(img_paths, total=len(img_paths)))
 
+if __name__ == '__main__':
+    patch_size = 800  # 切分patch的大小
 
-def move_fire(ori_path, save_path, suffix=""):
-    img_dir = Path(ori_path) / "img"
-    label_dir = Path(ori_path) / "labelcol"
+    # ---------------------------  训练集
+    # pos_dataset_path = r"D:\组会内容\data\Digestpath2019\Colonoscopy_tissue_segment_dataset\pos"
+    # pos_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800/pos_800"     # 输出地址
 
-    save_img_dir = Path(save_path) / ("img" + suffix)
-    save_img_dir.mkdir(exist_ok=True)
-    save_label_dir = Path(save_path) / ("labelcol" + suffix)
-    save_label_dir.mkdir(exist_ok=True)
-    # 转移img
-    for fire in sorted(img_dir.iterdir()):
-        img_path = save_img_dir / fire.name
-        shutil.copyfile(fire, img_path)  # 拷贝文件
+    # neg_dataset_path = r"D:\组会内容\data\Digestpath2019\Colonoscopy_tissue_segment_dataset\neg"
+    # neg_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800/neg_800"     # 输出地址
+    # ----------------------------
 
-    # 转移label
-    for fire in sorted(label_dir.iterdir()):
-        img_path = save_label_dir / fire.name
-        shutil.copyfile(fire, img_path)  # 拷贝文件
+    # --------------------------- 测试集
+    pos_dataset_path = r"D:\组会内容\data\Digestpath2019\tissue-testset-final\pos"
+    pos_output = r"D:\组会内容\data\Digestpath2019\MedT\val\only_mask/pos_800"  # 输出地址
 
-
-# pos_dataset_path = r"D:\组会内容\data\Digestpath2019\Colonoscopy_tissue_segment_dataset\pos"
-# pos_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800/pos_800"     # 输出地址
-
-# neg_dataset_path = r"D:\组会内容\data\Digestpath2019\Colonoscopy_tissue_segment_dataset\neg"
-# neg_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800/neg_800"     # 输出地址
-
-# 测试集
-pos_dataset_path = r"D:\组会内容\data\Digestpath2019\tissue-testset-final\pos"
-pos_output = r"D:\组会内容\data\Digestpath2019\MedT\val\only_mask/pos_800"  # 输出地址
-
-neg_dataset_path = r"D:\组会内容\data\Digestpath2019\tissue-testset-final\neg"
-neg_output = r"D:\组会内容\data\Digestpath2019\MedT\val\only_mask/neg_800"  # 输出地址
-
-# save_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800"     # 合并输出地址
-
-patch_size = 800  # 切分patch的大小
-# patch_resize = 256                                 # 切分完后patch,再resize成多大;
-
-fun(pos_dataset_path, pos_output, class_type="pos", need_filter=True)
-fun(neg_dataset_path, neg_output, class_type="neg", need_filter=True)
-
-# print("合并输出___")
-# move_fire(ori_path = pos_output,save_path = save_output,suffix="")  # 转移pos
-# move_fire(ori_path = neg_output,save_path = save_output,suffix="")  # 转移neg
+    neg_dataset_path = r"D:\组会内容\data\Digestpath2019\tissue-testset-final\neg"
+    neg_output = r"D:\组会内容\data\Digestpath2019\MedT\val\only_mask/neg_800"  # 输出地址
+    # ---------------------------
 
 
-# print('\nSplitting into patches 分裂成补丁 ...')
-# executor = Parallel(n_jobs=8)
-# executor(delayed(process_img_and_mask)(img_path, mask_path)
-#          for img_path, mask_path in tqdm(zip(img_paths, mask_paths), total=len(img_paths)))
+    run(pos_dataset_path, pos_output, class_type="pos", need_filter=True)
+    run(neg_dataset_path, neg_output, class_type="neg", need_filter=True)
 
-
-# executor(delayed(process_img_and_mask_neg)(img_path)
-#          for img_path in tqdm(img_paths, total=len(img_paths)))
-# for img_path in tqdm(img_paths, total=len(img_paths)):
-#     process_img_and_mask_neg(img_path)
+    # save_output =r"D:\组会内容\data\Digestpath2019\MedT\train\only_mask\train_800"     # 合并输出地址
+    # print("合并输出___")
+    # move_fire(ori_path = pos_output,save_path = save_output,suffix="")  # 转移pos
+    # move_fire(ori_path = neg_output,save_path = save_output,suffix="")  # 转移neg
