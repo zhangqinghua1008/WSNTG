70cb0248b4215e083880b9cb9aef3a42f0214b75
zqh <630762329@qq.com>
2021-12-05 17:22:56 +0800

    pos 和 neg 两种样本测试

--- test/infer_test_tile.py
+++ test/infer_test_tile.py
@@ -39,34 +39,45 @@ def pixel_infer(model,data_dir,patch_size,resize_size=None,device='cuda',output_
                 save_pre(pre, img_path, output_dir / "_pre")
 
 
-# 执行超像素推理
-def run():
-    data_dir = r"D:\组会内容\data\Digestpath2019\MedT\test/"
-    checkpoint = r"D:\组会内容\实验报告\MedT\records\20211106-1339-PM\checkpoints/ckpt.0054.pth"
-    model_type = 'wesup'
+# 执行模型正常推理
+def infer_run(model_type = None):
+    checkpoint = Path(r"D:\组会内容\实验报告\MedT\records\20211203-2257-PM_sizeof\checkpoints/ckpt.0200.pth")
     patch_size = 800
-    resize_size = 280  # None
+    resize_size = 400  # None
 
-    output_dir = r"D:\组会内容\实验报告\MedT\records\Digestpath_WSI_results_WESUP/temp"
+    test_model = 'all'  # fast || all
 
-    # 加载模型
+    output_dir = checkpoint.parent.parent / 'results'
+    output_dir.mkdir(exist_ok=True)
+
+    # 10张图快速测试
+    if test_model == 'fast':
+        data_dir = r"D:\组会内容\data\Digestpath2019\MedT\test\fast_test/"
+        output_dir = output_dir / "fast_test"
+        output_dir.mkdir(exist_ok=True)
+    # 完整90张测试图片
+    elif test_model == 'all':
+        data_dir = r"D:\组会内容\data\Digestpath2019\MedT\test\all_test/"
+        output_dir = output_dir / "all_test"
+        output_dir.mkdir(exist_ok=True)
+
+    # 加载模型并推理
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     trainer = initialize_trainer(model_type, device=device)
     if checkpoint is not None:
         trainer.load_checkpoint(checkpoint)
-
     infer(trainer,data_dir,patch_size = patch_size ,output_dir=output_dir,resize_size=resize_size,device=device)
 
-    # 测指标
-    pre_dir = output_dir + "/_post"
-    lable_dir = r"D:\组会内容\data\Digestpath2019\MedT\test\labelcol"
-    performance_metrics(pre_dir,lable_dir)
+    # -- 测指标
+    print("\ncheckpoint:", checkpoint, "\n")
+    lable_dir = data_dir + "/labelcol"  # GT 地址
+    evaluate_img(modelPre_dir=output_dir, gt_lable_dir=lable_dir, need_post=True)  # 评价模型输出
 
 
 # 执行像素级别推理
-def pixel_run():
-    checkpoint = Path(r"D:\组会内容\实验报告\MedT\records\20211201-1034-AM\checkpoints/ckpt.0020.pth")
-    model_type = 'tgcn'  # wesup / tgcn
+def infer_pixel_run(model_type = 'tgcn'):  # wesup / tgcn
+    begin = time.time()
+    checkpoint = Path(r"D:\组会内容\实验报告\MedT\records\20211205-0043-AM\checkpoints/ckpt.0025.pth")
     patch_size = 800
     resize_size = 280  # None
 
@@ -77,12 +88,12 @@ def pixel_run():
 
     # 10张图快速测试
     if test_model =='fast':
-        data_dir = r"D:\组会内容\data\Digestpath2019\MedT\test\fast_test/"
+        data_dir = Path(r"D:\组会内容\data\Digestpath2019\MedT\test\fast_test/")
         output_dir = output_dir / "fast_test"
         output_dir.mkdir(exist_ok=True)
     # 完整90张测试图片
     elif test_model == 'all':
-        data_dir = r"D:\组会内容\data\Digestpath2019\MedT\test\all_test/"
+        data_dir = Path(r"D:\组会内容\data\Digestpath2019\MedT\test\all_test/")
         output_dir = output_dir / "all_test"
         output_dir.mkdir(exist_ok=True)
 
@@ -96,17 +107,24 @@ def pixel_run():
         model.load_state_dict(torch.load(checkpoint)['model_state_dict'])
 
     # 推理
-    pixel_infer(model,data_dir,patch_size = patch_size ,output_dir=output_dir,resize_size=resize_size,device=device)
+    phases = ["pos","neg"]
+    for phase in phases:
+        print("\n++++++++++++++++++++ 当前正在处理：",phase)
+        phase_data_dir = data_dir / phase
+        phase_output_dir = output_dir / phase
+        phase_output_dir.mkdir(exist_ok=True)
+        pixel_infer(model, phase_data_dir, patch_size=patch_size, output_dir=phase_output_dir, resize_size=resize_size,
+                    device=device)
 
-    print("\ncheckpoint:",checkpoint,"\n")
-    # GT 地址
-    lable_dir = data_dir + "/labelcol"
-    # 评价模型输出
-    evaluate_img(modelPre_dir = output_dir, gt_lable_dir = lable_dir,need_post = True)
+        print("\ncheckpoint:", checkpoint, "\n")
+        # 评价模型输出
+        phase_lable_dir = phase_data_dir / "labelcol"  # GT 地址
+        evaluate_img(modelPre_dir=phase_output_dir, gt_lable_dir=phase_lable_dir, need_post=False)
 
 
 if __name__ == '__main__':
-    # 执行像素级别推理
-    pixel_run()
+    # 执行像素级别推理 （只有tgcn 和 wesup需要）
+    infer_pixel_run(model_type='tgcn')
 
-    # run()
+    # 正常模型推理
+    # infer_run(model_type='sizeloss')  # cdws / sizeloss
